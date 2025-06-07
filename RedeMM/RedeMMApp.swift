752affd31cf1ba0d08adacac228d98cbd35682bb
Rda <rda.dr@icloud.com>
2025-06-07 01:32:32 -0300

    Initial Commit

---
+++ RedeMM/RedeMMApp.swift
@@ -0,0 +1,32 @@
+//
+//  RedeMMApp.swift
+//  RedeMM
+//
+//  Created by rafael dalazen araujo on 07/06/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct RedeMMApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
